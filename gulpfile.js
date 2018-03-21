36c7007e1616949e3b8709e3c4232bc2dca49e59
Junyi Yi <junyi@microsoft.com>
2018-03-20 18:07:47 -0700

    Add HDInsight sample for the generator.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 const gulp = require('gulp');
 const { 
     clean: dotnet_clean,
     build: dotnet_build
 } = require('gulp-dotnet-cli');
+const shrun = require('gulp-run');
 
 gulp.task('clean', () => {
     return gulp.src('*.sln', { read: false })
@@ -17,4 +20,26 @@ gulp.task('build:debug', () => {
 gulp.task('build', ['clean'], () => {
     return gulp.src('*.sln', { read: false })
         .pipe(dotnet_build({ configuration: 'Release' }));
+});
+
+
+function getAutoRestCommand(config) {
+    const plugin = path.resolve('.').replace(/\\/g, '\\\\');
+    const metadata = path.resolve(config.metadata).replace(/\\/g, '\\\\');
+    const output = path.resolve(metadata, '../generated').replace(/\\/g, '\\\\');
+    const commands = ['autorest',
+        '--terraform-provider', '--use="' + plugin + '"',
+        '--input-file="' + config.input + '"',
+        '--namespace="' + config.namespace + '"',
+        '--terraform-provider.metadata-file="' + metadata + '"',
+        '--clear-output-folder', '--can-clear-output-folder', '--output-folder="' + output + '"'];
+    if (config.displayModel) {
+        commands.push('--terraform-provider.display-model')
+    }
+    return commands.join(' ');
+}
+
+gulp.task('run:hdinsight', () => {
+    const config = require('./samples/hdinsight/autorest');
+    return shrun(getAutoRestCommand(config)).exec();
 });
